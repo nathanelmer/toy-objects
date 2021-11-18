67b0d898595ef41131577cbefb72c0f795e3fc41
nathanelmer <nelmer12@gmail.com>
2021-11-18 13:33:37 -0600

    initial commit

---
+++ leonids-toys.js
@@ -0,0 +1,40 @@
+const toys = [
+    {
+    id : 1,
+    type : "Race Car", 
+    maker : "Toys R Us",
+    price : 21
+},
+{
+    id : 2,
+    type : "Action Figure",
+    maker : "Target", 
+    price : 10
+},
+{
+    id : 3, 
+    type : "Stuffed Animal", 
+    maker : "Care Bear", 
+    price : 30
+}
+]
+
+let wantedToy = 2
+let budget = 25
+
+for (const toy of toys){
+    toy.price += 0.5
+}
+
+for (const toy of toys){
+    if (toy.id === wantedToy){
+        console.log(`${toy.maker} made my ${toy.type}, and it costed $${toy.price}.`)
+    }   
+}
+
+for (toy of toys){
+    if(toy.price <= budget){
+        console.log("We have found 1 option!")
+    }    
+}
+
